b1138deb922b7d6764a6aab2e07f0984727a8220
jdietsch31 <48184520+jdietsch31@users.noreply.github.com>
2022-02-06 12:35:47 +0000

    commitone

--- static/app.js
+++ static/app.js
@@ -15,43 +15,42 @@ function UserListViewModel() {
     self.password= ko.observable();
 
     self.addUser = function() {
-        self.save();
-      self.name("");
-        self.username("");
-        self.email("");
-      self.password("");
+	self.save();
+  self.name("");
+	self.username("");
+	self.email("");
+  self.password("");
     };
 
     $.getJSON('/api/v1/users', function(userModels) {
-        var t = $.map(userModels.user_list, function(item) {
-            return new User(item);
-        })
-        self.user_list(t);
-    })
-    
-    self.save = function() {
-    return $.ajax({
-        url:'/api/v1/users',
-        contentType: 'application/json',
-        type: 'POST',
-        data: JSON.stringify({
-            'name': self.name(),
-            'username': self.username(),
-        'email': self.email(),
-        'password': self.password()
-        }),
-        success: function(data) {
-            alert("success")
-            console.log("Pushing to users array");
-            self.push(new User({name: data.name, username: data.username,
-                 email: data.email, password: data.password}));
-            return;
-        },
-        error: function() {
-            return console.log("failed");
-        }
+	var t = $.map(userModels.user_list, function(item) {
+	    return new User(item);
+	});
+	self.user_list(t);
     });
+
+    self.save = function() {
+	return $.ajax({
+	    url: '/api/v1/users',
+	    contentType: 'application/json',
+	    type: 'POST',
+	    data: JSON.stringify({
+		'name': self.name(),
+		'username': self.username(),
+    'email': self.email(),
+    'password': self.password()
+	    }),
+	    success: function(data) {
+          alert("success")
+		      console.log("Pushing to users array");
+		      self.push(new User({ name: data.name, username: data.username,email: data.email ,password: data.password}));
+		      return;
+	    },
+	    error: function() {
+		return console.log("Failed");
+	    }
+	});
     };
 }
 
-ko.applyBindings( new UserListViewModel());
+ko.applyBindings(new UserListViewModel());
